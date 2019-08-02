f936f3a952f709df216c3f1362f6e3747829a000
Silas Alves <silas.alves@gmail.com>
2019-08-02 16:10:24 -0400

    Change tensor3d to canvas element
    The tensor3d object was slowing down the image processing, so it was
    changed to a canvas object.

--- scripts/test-posenet.js
+++ scripts/test-posenet.js
@@ -7,6 +7,7 @@ require('@tensorflow/tfjs-node');
 const posenet = require('@tensorflow-models/posenet');
 
 const cv = require('opencv4nodejs');
+const { createImageData, createCanvas } = require('canvas')
 
 async function main() {
     let paramName = '/posenet';
@@ -30,6 +31,9 @@ async function main() {
     const net = await posenet.load();
     rosnodejs.log.info('PoseNet model loaded.');
 
+    const imgCanvas = createCanvas(640, 480);
+    const imgCtx = imgCanvas.getContext('2d');
+
     let options = {queueSize: 1, throttleMs: 100};
     const imgSub = rosNode.subscribe(paramImgNode, sensor_msgs.Image, async (imgData) => {
         let img = undefined;
@@ -37,28 +41,23 @@ async function main() {
         if(imgData.encoding == "rgb8")
             img = imgData.data;
         else if(imgData.encoding == "bgr8")
-            img = new cv.Mat(Buffer.from(imgData.data), imgData.height, imgData.width, cv.CV_8UC3).cvtColor(cv.COLOR_BGR2RGB);
+            img = new cv.Mat(Buffer.from(imgData.data), imgData.height, imgData.width, cv.CV_8UC3).cvtColor(cv.COLOR_BGR2RGBA);
 
         //cv.imshow('test', img);
         //cv.waitKey(1);
 
         if(img != undefined){
             console.time("posenet")
-            let tensor = tf.tensor3d(img.getDataAsArray(), [imgData.height,imgData.width,3], 'int32');
-            // net.estimateSinglePose(tensor, paramScaleFactor, paramFlipHorizontal, paramOutputStride)
-            // .then((pose) => {
-            //     if(pose['score'] > 0.2){
-            //         for(let k = 0; k < pose['keypoints'].length; k++){
-            //             if(pose['keypoints'][k]['score'] > 0.2)
-            //                 img.drawCircle(new cv.Point(pose['keypoints'][k]['position']['x'], pose['keypoints'][k]['position']['y']),
-            //                 4, new cv.Vec3(255, 0, 0), 2, 8, 0);
-            //         }
-            //     }
-            //     cv.imshow('test', img.cvtColor(cv.COLOR_RGB2BGR));
-            //     cv.waitKey(1);
-            // });
+            let tempImg = createImageData(
+                new Uint8ClampedArray(img.getData()),
+                imgData.width,
+                imgData.height
+            );
+            imgCanvas.height = imgData.height;
+            imgCanvas.width = imgData.width;
+            imgCtx.putImageData(tempImg, 0, 0);
 
-            pose = await net.estimateSinglePose(tensor, paramScaleFactor, paramFlipHorizontal, paramOutputStride);
+            pose = await net.estimateSinglePose(imgCanvas, paramScaleFactor, paramFlipHorizontal, paramOutputStride);
             if(pose['score'] > 0.2){
                 for(let k = 0; k < pose['keypoints'].length; k++){
                     if(pose['keypoints'][k]['score'] > 0.2)
