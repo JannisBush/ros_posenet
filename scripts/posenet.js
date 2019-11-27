1cf645e6e6a865a6f5950f2db3a6325d1a3a2ccc
Silas Alves <silas.alves@gmail.com>
2019-11-27 14:17:56 -0800

    Fixes several bugs.
    - Fix debug viewer;
    
    - Adds a parameter to allow posenet to discard old messages;
    
    - Changes the forEach loops to classic for loops in `buildOutputMessage`
    to increase performance.

--- scripts/posenet.js
+++ scripts/posenet.js
@@ -28,6 +28,47 @@ const { createImageData, createCanvas } = require('canvas')
 // detected poses will be shown.
 const DEBUG = false;
 
+const nose = 0
+const leftEye = 1
+const rightEye = 2
+const leftEar = 3
+const rightEar = 4
+const leftShoulder = 5
+const rightShoulder = 6
+const leftElbow = 7
+const rightElbow = 8
+const leftWrist = 9
+const rightWrist = 10
+const leftHip = 11
+const rightHip = 12
+const leftKnee = 13
+const rightKnee = 14
+const leftAnkle = 15
+const rightAnkle = 16
+
+const connected_part_names = [
+    [leftHip, leftShoulder], [leftElbow, leftShoulder],
+    [leftElbow, leftWrist], [leftHip, leftKnee],
+    [leftKnee, leftAnkle], [rightHip, rightShoulder],
+    [rightElbow, rightShoulder], [rightElbow, rightWrist],
+    [rightHip, rightKnee], [rightKnee, rightAnkle],
+    [leftShoulder, rightShoulder], [leftHip, rightHip]]
+
+const skeleton_colors=[
+    new cv.Vec3(0, 204, 102),
+    new cv.Vec3(204, 102, 0),
+    new cv.Vec3(153, 0, 153),
+    new cv.Vec3(0, 128, 255),
+    new cv.Vec3(102, 102, 255),
+    new cv.Vec3(128, 128, 128),
+    new cv.Vec3(102, 0, 51),
+    new cv.Vec3(0, 0, 153),
+    new cv.Vec3(102, 102, 0),
+    new cv.Vec3(153, 51, 255),
+    new cv.Vec3(102, 0, 0),
+    new cv.Vec3(0, 0, 0)
+]
+
 /**
  * Transforms a ROS image message into a Canvas object, so that it can be
  * inputted into the PoseNet neural network.
@@ -44,12 +85,12 @@ function formatImage(imgData){
         conversionCode = cv.COLOR_BGR2RGBA;
     else
         throw "Unknown image format.";
-
+    
     let img = new cv.Mat(Buffer.from(imgData.data), imgData.height, 
                 imgData.width, cv.CV_8UC3).cvtColor(conversionCode);
     
     // Creates the Canvas object, draw and return it.
-    const imgCanvas = createCanvas(imgData.height, imgData.width);
+    const imgCanvas = createCanvas(imgData.width, imgData.height);
     const imgCtx = imgCanvas.getContext('2d');
     let tempImg = createImageData(
         new Uint8ClampedArray(img.getData()),
@@ -60,7 +101,7 @@ function formatImage(imgData){
     return imgCanvas;
 }
 
-
+    
 /**
  * Draws the detected keypoints over the input image and shows to the user.
  * 
@@ -68,30 +109,51 @@ function formatImage(imgData){
  * @param {sensor_msgs.Image} imgData A ROS image message.
  * @param {Pose} poses The poses detected by PoseNet.
  */
-function debugView (imgData, poses) {
+function debugView (imgData, poses, minPoseScore, minPartScore) {
     if(imgData.encoding == "rgb8")
-        conversionCode = cv.COLOR_RGB2RGBA;
+        conversionCode = cv.COLOR_RGB2BGR;
     else if(imgData.encoding == "bgr8")
-        conversionCode = cv.COLOR_BGR2RGBA;
+        conversionCode = null;
     else
         throw "Unknown image format.";
         
     img = new cv.Mat(Buffer.from(imgData.data), imgData.height, 
-            imgData.width, cv.CV_8UC3).cvtColor(conversionCode);
+            imgData.width, cv.CV_8UC3);
     
-    poses.forEach( pose => {
-        if(pose['score'] > 0.2){
+    if(conversionCode != null)
+        img = img.cvtColor(conversionCode);
+    
+    console.log(Object.keys(poses).length)
+    
+    poses.forEach( function(pose, i) {
+        if (pose['score'] < minPoseScore)
+            return;
+
+        if (i>11)
+            i = 11;
+        color = skeleton_colors[i]
+
+        connected_part_names.forEach( pair => {
+            if (pose['keypoints'][pair[0]]['score'] > 0.2 && pose['keypoints'][pair[1]]['score'] > 0.2) {
+                let p0 = pose['keypoints'][pair[0]]['position']
+                let p1 = pose['keypoints'][pair[1]]['position']
+                img.drawLine(new cv.Point(p0['x'], p0['y']), new cv.Point(p1['x'], p1['y']),
+                    color, 8);
+            }
+        });
+
+        if(pose['score'] > minPartScore){
             pose['keypoints'].forEach(keypoint => {
                 if(keypoint['score'] > 0.2)
                     img.drawCircle(new cv.Point(
                         keypoint['position']['x'], 
                         keypoint['position']['y']),
-                    4, new cv.Vec3(129, 245, 60), 2, 8, 0);
+                    20, color, 5, 8, 0);
             });
         }
     });
 
-    cv.imshow('test', img.cvtColor(cv.COLOR_RGB2BGR));
+    cv.imshow('test', img)
     cv.waitKey(1);
 }
 
@@ -112,6 +174,7 @@ async function main() {
     const paramImgTopic = await getParam('/posenet/image_topic', '/image_raw');
     const paramPosesTopic = await getParam('/posenet/poses_topic', '/poses');
     const paramGPU = await getParam('/posenet/gpu', false);
+    const paramMaxDelay = await getParam('posenet/max_msg_time_diff', 0.03);
     const paramArchitecture = await getParam('/posenet/architecture', 'MobileNetV1');
     const paramMultiplier = await getParam('/posenet/multiplier', 0.5);
     let paramInputResolution = await getParam('/posenet/input_resolution', 257);
@@ -146,7 +209,7 @@ async function main() {
 
     // Creates the publishing topic and subscribe to the image topic.
     let posePub = rosNode.advertise(paramPosesTopic, pose_msgs.Poses);
-    let options = {queueSize: 1, throttleMs: 100};
+    let options = {queueSize: 1};
     let imgSub;
     if (paramMultiPose)
         imgSub = rosNode.subscribe(paramImgTopic, sensor_msgs.Image, 
@@ -184,6 +247,13 @@ async function main() {
      * @param {sensors_msgs.Image} imgData A ROS image message.
      */
     async function singlePoseCallback(imgData){
+        let t0 = rosnodejs.Time.toSeconds(imgData.header.stamp)
+        let t1 = rosnodejs.Time.toSeconds(rosnodejs.Time.now())
+        if(DEBUG)
+            console.log("Delay between messages: %f", t1-t0);
+        if((t1 - t0) > paramMaxDelay)
+            return;
+        
         const imgCanvas = formatImage(imgData);
         if(DEBUG)
             console.time("posenet")
@@ -191,11 +261,10 @@ async function main() {
             {flipHorizontal: paramFlipHorizontal});
         if(DEBUG){
             console.timeEnd("posenet");
-            debugView(imgData, [pose]);
+            debugView(imgData, [pose], paramMinPoseConf, paramMinPartConf);
         }
         
-        posePub.publish(buildOutputMessage([pose]));
-        
+        posePub.publish(buildOutputMessage([pose], imgData.header));
     }
 
 
@@ -207,20 +276,28 @@ async function main() {
      * @param {sensors_msgs.Image} imgData A ROS image message.
      */
     async function multiPoseCallback(imgData){
-        const imgCanvas = formatImage(imgData);
+        let t0 = rosnodejs.Time.toSeconds(imgData.header.stamp)
+        let t1 = rosnodejs.Time.toSeconds(rosnodejs.Time.now())
+        if(DEBUG)
+            console.log("Delay between messages: %f", t1-t0);
+        if((t1 - t0) > paramMaxDelay)
+            return;
+
         if(DEBUG)
             console.time("posenet")
+
+        const imgCanvas = formatImage(imgData);
         poses = await net.estimateMultiplePoses(imgCanvas, {
             flipHorizontal: paramFlipHorizontal,
             maxDetections: paramMaxDetection,
             scoreThreshold: paramMinPartConf,
-            nmsRadius: paramNmsRadius
-        });
+            nmsRadius: paramNmsRadius});
+            
         if(DEBUG){
             console.timeEnd("posenet");
-            debugView(imgData, poses);
+            debugView(imgData, poses, paramMinPoseConf, paramMinPartConf);
         }
-        posePub.publish(buildOutputMessage(poses));
+        posePub.publish(buildOutputMessage(poses, imgData.header));
     }
 
 
@@ -229,23 +306,25 @@ async function main() {
      * @param {[Poses]} poses The poses outputted by PoseNet.
      * @returns {pose_msgs.Poses} The ROS message that will be published.
      */
-    function buildOutputMessage(poses) {
+    function buildOutputMessage(poses, header) {
         let msg = new pose_msgs.Poses();
-        poses.forEach(poseData => {
-            if (poseData['score'] > paramMinPoseConf) {
+        msg.header.stamp = header.stamp;
+        msg.header.frame_id = header.frame_id;
+        for(let pIdx = 0; pIdx < poses.length; pIdx++){
+            if (poses[pIdx]['score'] > paramMinPoseConf) {
                 pose = new pose_msgs.Pose();
-                pose.score = poseData['score'];
-                poseData['keypoints'].forEach(keypointData => {
+                pose.score = poses[pIdx]['score'];
+                for(let kIdx = 0; kIdx < poses[pIdx]['keypoints'].length; kIdx++){
                     keypoint = new pose_msgs.Keypoint();
-                    keypoint.score = keypointData['score'];
-                    keypoint.part = keypointData['part'];
-                    keypoint.position.x = keypointData['position']['x'];
-                    keypoint.position.y = keypointData['position']['y'];
+                    keypoint.score = poses[pIdx]['keypoints'][kIdx]['score'];
+                    keypoint.part = poses[pIdx]['keypoints'][kIdx]['part'];
+                    keypoint.position.x = poses[pIdx]['keypoints'][kIdx]['position']['x'];
+                    keypoint.position.y = poses[pIdx]['keypoints'][kIdx]['position']['y'];
                     pose.keypoints.push(keypoint);
-                });
+                };
                 msg.poses.push(pose);
             }
-        });
+        };
         return msg;
     }
 }
